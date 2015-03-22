29c76ffc56ac0b2e192361d12ae575bfb2ba9b0b
Curtis Gibby <curtis@matchthememory.com>
2015-03-22 15:14:43 -0600

    make bottom submit button "btn-primary"

--- index.php
+++ index.php
@@ -275,7 +275,7 @@ if(isset($form['Date'])) {
 			</div>
 		</div>
 
-		<button type="submit" class="btn btn-default">Submit</button>
+		<button type="submit" class="btn btn-primary">Submit</button>
 		</form>
 
 		<?php endif;?>
