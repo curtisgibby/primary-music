e4b633844e1d89c6d7456408ae034d3d53717ead
Curtis Gibby <cgibby@gmail.com>
2017-01-12 20:07:28 -0700

    add German and Italian;

---
+++ locale/de.php
@@ -0,0 +1,38 @@
+<?php
+$GLOBALS = array(
+	'locale' => 'de_DE',
+	'language_code' => 'deu',
+	'labels' => array(
+		'LABEL_PAGE_TITLE'               => 'HLT-Primärmusikplaner',
+		'LABEL_NOT_OFFICIAL'             => 'Dies ist keine offizielle Website der Kirche Jesu Christi der Heiligen der Letzten Tage.',
+		'LABEL_EXPLANATION'              => "Diese Seite soll dazu beitragen, HLT Primary Music Leaders wählen Musik für ihre primären Treffen. Wählen Sie ein Datum (standardmäßig auf \"diesen Sonntag\"), dann rufen Sie durch die Song-Felder, geben Sie ein Wort oder eine Nummer und wählen Sie Songs aus dem Pop-up, und senden Sie das Formular. Dann drucken Sie die Ergebnisse oder senden Sie die URL in einer E-Mail an Ihre Begleiter oder Präsident­schaft der Primar­vereinigung.",
+		'LABEL_SUBMIT'                   => 'Einreichen',
+		'LABEL_RESET'                    => 'Zurücksetzen',
+		'LABEL_LANGUAGE'                 => 'Sprache',
+		'LABEL_DATE'                     => 'Termin',
+		'LABEL_SINGING_TIME'             => 'Miteinander',
+		'LABEL_SINGING_TIME_EXPLANATION' => 'Sie können so viele oder wenige Songs in diesem Abschnitt eingeben, wie Sie möchten.',
+		'LABEL_NOTES'                    => 'Aufzeichnungen',
+		'LABEL_NO_PRINT'                 => 'Sie möchten diese Seite wahrscheinlich nicht ausdrucken. Stattdessen können Sie den Druckvorgang abbrechen und auf die Schaltfläche "Einreichen" klicken, um eine schöne Seite für den Druck zu erhalten.',
+		'LABEL_SHARE'                    => 'Diesen Plan teilen',
+		'LABEL_BACK'                     => 'Zurückgehen',
+	),
+	'collections' => array(
+		'default' => 'lieder-fur-kinder',
+		'LK' => 'liederbuch-fur-kinder',
+		'Gesangbuch' => 'hymnal',
+	),
+	'options' => array(
+		'OPTION_PRELUDE' => 'Lied des Vorspiels',
+		'OPTION_WELCOME' => 'Willkommenes Lied',
+		'OPTION_OPENING' => 'Eröffnungslied',
+		'OPTION_BIRTHDAY' => 'Lied des Geburtstages',
+		'OPTION_BAPTISM' => 'Tauflied',
+		'OPTION_ARTICLE' => 'Glaubensartikels Lied',
+		'OPTION_SCRIPTURE' => 'Schriftslied',
+		'OPTION_SHARING' => 'Miteinander Lied',
+		'OPTION_REVERENCE' => 'Ehrfurchtslied',
+		'OPTION_WIGGLE' => 'Wackelndes Lied',
+		'OPTION_CLOSING' => 'Schlusslied',
+	)
+);
---
+++ locale/it.php
@@ -0,0 +1,38 @@
+<?php
+$GLOBALS = array(
+	'locale' => 'it_IT',
+	'language_code' => 'ita',
+	'labels' => array(
+		'LABEL_PAGE_TITLE'               => 'Pianificatore di Musica della primaria SUG',
+		'LABEL_NOT_OFFICIAL'             => 'Questo non è un sito ufficiale della Chiesa di Gesù Cristo dei Santi degli Ultimi Giorni.',
+		'LABEL_EXPLANATION'              => "Questa pagina ha lo scopo di aiutare i dirigenti della musica LDS Primaria selezionare la musica per le loro riunioni primarie. Scegliere una data (il default è \"questa Domenica\"), quindi la scheda attraverso i campi di canzoni, digitando una parola o un numero e raccogliendo i brani dal pop-up, e inviare il modulo. Poi stampare i risultati o inviare l'URL in una e-mail ai vostri accompagnatori o presidenza della Primaria.",
+		'LABEL_SUBMIT'                   => 'Invio',
+		'LABEL_RESET'                    => 'Reset',
+		'LABEL_LANGUAGE'                 => 'Lingua',
+		'LABEL_DATE'                     => 'Data',
+		'LABEL_SINGING_TIME'             => 'Attività di gruppo',
+		'LABEL_SINGING_TIME_EXPLANATION' => 'È possibile inserire tante o poche canzoni in questa sezione come ti piace.',
+		'LABEL_NOTES'                    => 'Note',
+		'LABEL_NO_PRINT'                 => 'Probabilmente non si desidera stampare questa pagina. Piuttosto, annullare la stampa e premere il pulsante di invio per ottenere una bella pagina formattata per la stampa.',
+		'LABEL_SHARE'                    => 'Condividi questo piano',
+		'LABEL_BACK'                     => 'Torna indietro',
+	),
+	'collections' => array(
+		'default' => '',
+		'IB'      => '',
+		'Innos'  => '',
+	),
+	'options' => array(
+		'OPTION_PRELUDE' => 'Canzone di chiusura',
+		'OPTION_WELCOME' => 'Canzone di benvenuto',
+		'OPTION_OPENING' => 'Canzone di apertura',
+		'OPTION_BIRTHDAY' => 'Canzone di compleanno',
+		'OPTION_BAPTISM' => 'Canzone del battesimo',
+		'OPTION_ARTICLE' => 'Canzone di articolo di fede',
+		'OPTION_SCRIPTURE' => 'Canzone di scrittura',
+		'OPTION_SHARING' => 'Canzone di le attività di gruppo',
+		'OPTION_REVERENCE' => 'Canzone di Riverenza',
+		'OPTION_WIGGLE' => 'Canzone per muoversi',
+		'OPTION_CLOSING' => 'Canzone di chiusura',
+	)
+);
